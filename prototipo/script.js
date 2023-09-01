82b91bcd05f8cef3861d0351f7e0a27725493e50
roboberto1403 <104931705+roboberto1403@users.noreply.github.com>
2023-09-01 11:39:16 -0300

    pagina central e barra de pesquisas corrigidas

--- prototipo/script.js
+++ prototipo/script.js
@@ -1,6 +1,8 @@
-// script.js
+/* script.js */
 const countrySelect = document.getElementById('countrySelect');
 const newsContainer = document.getElementById('newsContainer');
+const countryInput = document.getElementById('countryInput'); // Novo elemento de entrada para pesquisa de países
+const suggestions = document.getElementById('suggestions'); // Elemento para exibir sugestões
 
 const API_KEY = 'e2795791a80d4d3b93e50de8a80b1e62'; // Substitua pelo seu API Key
 const API_URL = 'https://newsapi.org/v2/top-headlines';
@@ -29,13 +31,31 @@ async function displayNews(countryCode) {
     });
 }
 
-countrySelect.addEventListener('change', () => {
-    const countryCode = countrySelect.value;
-    displayNews(countryCode);
+// Evento de digitação para atualizar as sugestões enquanto digita
+countryInput.addEventListener('input', () => {
+    const inputText = countryInput.value.toLowerCase();
+    const filteredCountries = countries.filter(country =>
+        country.toLowerCase().includes(inputText)
+    );
+
+    // Limpa as sugestões atuais
+    suggestions.innerHTML = '';
+
+    // Adiciona as sugestões à lista
+    filteredCountries.forEach(country => {
+        const suggestionItem = document.createElement('div');
+        suggestionItem.textContent = country;
+        suggestionItem.addEventListener('click', () => {
+            countryInput.value = country;
+            suggestions.innerHTML = ''; // Limpa as sugestões quando um país é selecionado
+            displayNews(country); // Exibe notícias do país selecionado
+        });
+        suggestions.appendChild(suggestionItem);
+    });
 });
 
-// Evento de digitação para atualizar as notícias enquanto digita o país
-countrySelect.addEventListener('input', () => {
+// Evento de seleção de país para exibir as notícias
+countrySelect.addEventListener('change', () => {
     const countryCode = countrySelect.value;
     displayNews(countryCode);
 });
@@ -43,4 +63,5 @@ countrySelect.addEventListener('input', () => {
 // Carrega as notícias ao carregar a página com um país padrão (por exemplo, "US" para os Estados Unidos)
 const defaultCountry = 'US';
 displayNews(defaultCountry);
+
 
