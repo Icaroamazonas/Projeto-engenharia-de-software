188391776e4cd2bd833d9d440cfdce1406d4bc69
roboberto1403 <104931705+roboberto1403@users.noreply.github.com>
2023-09-04 16:11:22 -0300

    seção geral adicionada

--- prototipo/script.js
+++ prototipo/script.js
@@ -19,12 +19,6 @@ async function fetchRandomCountryCode(l) {
     return randomCountryCodes;
 }
 
-async function fetchNews(countryCode) {
-    const response = await fetch(`${API_URL}?country=${countryCode}&apiKey=${API_KEY}`);
-    const data = await response.json();
-    return data.articles;
-}
-
 async function fetchNewsGeneral() {
     try {
         const countryCodes = await fetchRandomCountryCodes(3);
@@ -52,24 +46,6 @@ async function fetchNewsGeneral() {
     }
 }
 
-async function displayNews(countryCode) {
-    const news = await fetchNews(countryCode);
-
-    newsContainer.innerHTML = ''; // Limpa o conteúdo anterior
-
-    news.forEach(article => {
-        const newsItem = document.createElement('div');
-        newsItem.classList.add('news-item');
-        newsItem.innerHTML = `
-            <img src="${article.urlToImage}" alt="${article.title}">
-            <h2>${article.title}</h2>
-            <p>${article.description}</p>
-            <a href="${article.url}" target="_blank">Read More</a>
-        `;
-        newsContainer.appendChild(newsItem);
-    });
-}
-
 // Evento de digitação para atualizar as sugestões enquanto digita
 countryInput.addEventListener('input', async () => {
     const inputText = countryInput.value.toLowerCase();
@@ -92,7 +68,11 @@ countryInput.addEventListener('input', async () => {
     });
 });
 
-fetchNewsGeneral();
+// Evento de carregamento da página para buscar as notícias gerais
+window.addEventListener('load', () => {
+    fetchNewsGeneral();
+});
+
 
 
 
