365a71cc533008cf676c5559cd3962e2131ef19a
roboberto1403 <104931705+roboberto1403@users.noreply.github.com>
2023-09-04 19:54:31 -0300

    seção geral

--- prototipo/script.js
+++ prototipo/script.js
@@ -1,12 +1,10 @@
 /* script.js */
-const countryInput = document.getElementById('countryInput');
-const suggestions = document.getElementById('suggestions');
 const newsContainer = document.getElementById('newsContainer');
 const allCountryCodes = ['ae', 'ar', 'at', 'au', 'be', 'bg', 'br', 'ca', 'ch', 'cn', 'co', 'cu', 'cz', 'de', 'eg', 'fr', 'gb', 'gr', 'hk', 'hu', 'id', 'ie', 'il', 'in', 'it', 'jp', 'kr', 'lt', 'lv', 'ma', 'mx', 'my', 'ng', 'nl', 'no', 'nz', 'ph', 'pl', 'pt', 'ro', 'rs', 'ru', 'sa', 'se', 'sg', 'si', 'sk', 'th', 'tr', 'tw', 'ua', 'us', 've', 'za'];
 const API_KEY = 'e2795791a80d4d3b93e50de8a80b1e62';
 const API_URL = 'https://newsapi.org/v2/top-headlines';
 
-async function fetchRandomCountryCode(numberOfCodes) {
+async function fetchRandomCountryCodes(numberOfCodes) {
     const randomCountryCodes = [];
     while (randomCountryCodes.length < numberOfCodes) {
         const randomIndex = Math.floor(Math.random() * 54); // gera um numero aleatório que está dentro do tamanho da lista de países suportados pela NewsAPI (54)
