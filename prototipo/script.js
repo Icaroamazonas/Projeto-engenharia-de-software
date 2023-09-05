ce26543a5e4e10b2a3489b39e71cc119ede5bf75
roboberto1403 <104931705+roboberto1403@users.noreply.github.com>
2023-09-05 20:13:04 -0300

    categorias adicionadas

--- prototipo/script.js
+++ prototipo/script.js
@@ -1,5 +1,11 @@
 /* script.js */
-const newsContainer = document.getElementById('newsContainer');
+const generalNewsContainer = document.getElementById('category-general');
+const businessNewsContainer = document.getElementById('category-business');
+const entertainmentNewsContainer = document.getElementById('category-entertainment');
+const healthNewsContainer = document.getElementById('category-health');
+const sciencelNewsContainer = document.getElementById('category-science');
+const sportsNewsContainer = document.getElementById('category-sports');
+const technologyNewsContainer = document.getElementById('category-technology');
 const allCountryCodes = ['ae', 'ar', 'at', 'au', 'be', 'bg', 'br', 'ca', 'ch', 'cn', 'co', 'cu', 'cz', 'de', 'eg', 'fr', 'gb', 'gr', 'hk', 'hu', 'id', 'ie', 'il', 'in', 'it', 'jp', 'kr', 'lt', 'lv', 'ma', 'mx', 'my', 'ng', 'nl', 'no', 'nz', 'ph', 'pl', 'pt', 'ro', 'rs', 'ru', 'sa', 'se', 'sg', 'si', 'sk', 'th', 'tr', 'tw', 'ua', 'us', 've', 'za'];
 const API_KEY = 'e2795791a80d4d3b93e50de8a80b1e62';
 const API_URL = 'https://newsapi.org/v2/top-headlines';
@@ -17,22 +23,22 @@ async function fetchRandomCountryCodes(numberOfCodes) {
     return randomCountryCodes;
 }
 
-async function fetchNewsGeneral() {
-    const countryCodes = await fetchRandomCountryCodes(3);
+async function fetchNews(category, numberOfCodes) {
+    const countryCodes = await fetchRandomCountryCodes(numberOfCodes);
     const listOfArticles = [];
 
     for (const countryCode of countryCodes) {
-        const response = await fetch(`${API_URL}?country=${countryCode}&category=general&apiKey=${API_KEY}`);
+        const response = await fetch(`${API_URL}?country=${countryCode}&category=${category}l&apiKey=${API_KEY}`);
         const data = await response.json();
         listOfArticles.push(data.articles[0]); // Pega a primeira notícia
         }
     return listOfArticles;
 }
 
-async function displayNewsGeneral() {
-    const news = await fetchNewsGeneral();
+async function displayNews(category, categoryContainer, numberOfCodes) {
+    const news = await fetchNews(category, numberOfCodes);
 
-    newsContainer.innerHTML = ''; // Limpa o conteúdo anterior
+    categoryContainer.innerHTML = ''; // Limpa o conteúdo anterior
 
     news.forEach(article => {
         const newsItem = document.createElement('div');
@@ -41,15 +47,20 @@ async function displayNewsGeneral() {
             <img src="${article.urlToImage}" alt="${article.title}">
             <h2>${article.title}</h2>
             <p>${article.description}</p>
-            <a href="${article.url}" target="_blank">Read More</a>
+            <a href="${article.url}" target="_blank">Leia Mais</a>
         `;
-        newsContainer.appendChild(newsItem);
+        categoryContainer.appendChild(newsItem);
     });
 }
 
-displayNewsGeneral();
-
-
-
+window.addEventListener('load', () => {
+    displayNews('general', generalNewsContainer, 3);
+    displayNews('business', businessNewsContainer, 2);
+    displayNews('entertainment', entertainmentNewsContainer, 2);
+    displayNews('health', healthNewsContainer, 2)
+    displayNews('science', healthNewsContainer, 2)
+    displayNews('sports', sportsNewsContainer, 2)
+    displayNews('technology', technologyNewsContainer, 2)
+})
 
 
