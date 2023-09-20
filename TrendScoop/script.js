1d618a1285009cc93e049506d8b8b8be3ff8959f
Luiz Roberto <lrbf@cin.ufpe.br>
2023-09-20 14:29:47 -0300

    trendscoop global mas com sidebar bugada (começa aberta e não pode dar scroll down)

--- TrendScoop/script.js
+++ TrendScoop/script.js
@@ -15,8 +15,6 @@ const categoriaTitulo = document.getElementById('categoria-titulo');
 const categoriaAnterior = document.getElementById('categoria-anterior');
 const categoriaProxima = document.getElementById('categoria-proxima');
 
-const allCountryCodes = ['ae', 'ar', 'at', 'au', 'be', 'bg', 'br', 'ca', 'ch', 'cn', 'co', 'cu', 'cz', 'de', 'eg', 'fr', 'gb', 'gr', 'hk', 'hu', 'id', 'ie', 'il', 'in', 'it', 'jp', 'kr', 'lt', 'lv', 'ma', 'mx', 'my', 'ng', 'nl', 'no', 'nz', 'ph', 'pl', 'pt', 'ro', 'rs', 'ru', 'sa', 'se', 'sg', 'si', 'sk', 'th', 'tr', 'tw', 'ua', 'us', 've', 'za'];
-
 const API_KEY3 = '4a84afc4eb144b3f8106d1e9dbc25f1c';
 const API_KEY2 = '00715f1f32b94859aac4e24f0825a205';
 const API_KEY1 = 'e2795791a80d4d3b93e50de8a80b1e62';
@@ -26,39 +24,31 @@ const API_URL = 'https://newsapi.org/v2/top-headlines';
 // Objeto para armazenar em cache os resultados da API
 const displayedArticleTitles = {};
 
-// Objeto para armazenar as notícias filtradas por categoria
-const filteredNews = {
-    general: [],
-    business: [],
-    entertainment: [],
-    health: [],
-    science: [],
-    sports: [],
-    technology: [],
-};
-
 // Função para esperar um certo período de tempo antes de fazer a próxima solicitação
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// Função para buscar notícias, com cache e filtragem
-async function fetchNews(category, maxArticles) {
-
-    const response = await fetch(`${API_URL}?country=us&category=${category}&apiKey=${API_KEY3}`);
+async function fetchNews(country, category, maxArticles) {
+    const response = await fetch(`${API_URL}?country=${country}&category=${category}&apiKey=${API_KEY}`);
     const data = await response.json();
 
     // Filtra as notícias para conter no máximo `maxArticles` e ter imagem
-    const filteredArticles = data.articles.filter(article => {
+    let filteredArticles = data.articles.filter(article => {
         return article.urlToImage;
     }).slice(0, maxArticles);
+
+    // Se não houver artigos com imagem, procure por artigos sem imagem
+    if (filteredArticles.length === 0) {
+        filteredArticles = data.articles.slice(0, maxArticles);
+    }
 
     // Filtra novamente para excluir títulos já exibidos
     const uniqueArticles = filteredArticles.filter(article => {
         return !displayedArticleTitles[article.title];
     });
 
-    await delay(200);
+    await delay(500);
 
     // Atualiza o objeto de notícias filtradas
     filteredNews[category] = uniqueArticles;
@@ -70,6 +60,8 @@ async function fetchNews(category, maxArticles) {
 
     return uniqueArticles;
 }
+
+
 
 // Função para exibir notícias de uma categoria
 async function displayNews(category, categoryContainer) {
@@ -87,10 +79,11 @@ async function displayNews(category, categoryContainer) {
         } else {
             newsItem.classList.add('medium-news'); // Classe para estilizar a notícia grande em outras categorias
         }
+        const imageUrl = article.urlToImage ? article.urlToImage : 'https://i.pinimg.com/736x/f9/58/18/f95818f914844d2b1cf7a45b232061d1.jpg';
         newsItem.innerHTML = `
             <a href="${article.url}" target="_blank">
                 <div class="grid-image">
-                    <img src="${article.urlToImage}" alt="${article.title}">
+                    <img src="${imageUrl}" alt="${article.title}">
                 </div>
                 <h2 class="grid-title">${article.title}</h2>
             </a>
@@ -98,6 +91,28 @@ async function displayNews(category, categoryContainer) {
         categoryContainer.appendChild(newsItem);
     });
 }
+
+// Funções do sidebar
+function openSidebar(e){
+    document.getElementById("sidebar").style.width = "350px";
+    document.getElementsByTagName('body')[0].style.overflow = "hidden";
+}
+
+function closeSidebar(e){
+    document.getElementById("sidebar").style.width = "0px";
+    document.getElementsByTagName('body')[0].style.overflow = "unset";
+}
+
+function handleCountry(e) {
+    const selectedCountryCode = e.target.dataset['countryCode'];
+    const url = new URL(window.location.href); // Obtém a URL atual
+    url.searchParams.set('cc', selectedCountryCode); // Define o parâmetro do código do país na URL
+
+    // Atualiza a URL e recarrega a página
+    window.location.href = url.href;
+}
+//fim das funções do sidebar
+
 
 // Navegação entre categorias
 const categorias = ['business', 'entertainment', 'health', 'science', 'sports', 'technology'];
@@ -126,19 +141,36 @@ categoriaProxima.addEventListener('click', () => {
     displayNews(categoriaAtual, categoryOtherContainer);
 });
 
+
+const defaultCountry = 'US';
 // Carrega as notícias ao carregar a página com a categoria inicial
 window.addEventListener('load', async () => {
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const countryCodeParam = urlParams.get('cc') ? urlParams.get('cc') : defaultCountry;
+
+    // Limpa o objeto filteredNews antes de buscar notícias para o novo país
+    filteredNews = {
+        general: [],
+        business: [],
+        entertainment: [],
+        health: [],
+        science: [],
+        sports: [],
+        technology: [],
+    };
+    
     // Carrega notícias de várias categorias
-    await fetchNews('general', 3);
-    await fetchNews('technology', 5);
-    await fetchNews('business', 5);
-    await fetchNews('science', 5);
-    await fetchNews('entertainment', 5);
-    await fetchNews('health', 5);
-    await fetchNews('sports', 5);
+    await fetchNews(countryCodeParam,'general', 3);
+    await fetchNews(countryCodeParam,'technology', 5);
+    await fetchNews(countryCodeParam,'business', 5);
+    await fetchNews(countryCodeParam,'science', 5);
+    await fetchNews(countryCodeParam,'entertainment', 5);
+    await fetchNews(countryCodeParam,'health', 5);
+    await fetchNews(countryCodeParam,'sports', 5);
 
     // Exibe as notícias da categoria "geral"
     displayNews('general', categoryGeneralContainer);
     displayNews(categoriaAtual, categoryOtherContainer);
-
 });
+
